5fa8b0648efa9e8c06020692021fb33364883a4c
Luke Lemke <llemke777@gmail.com>
2023-10-25 19:34:43 -0400

    first commit

---
+++ let-constex.js
@@ -0,0 +1,19 @@
+
+
+//```jsx
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+//```
+
+const Pi = 3.14
+
+//## **Quiz**
+
+//- What is the difference between ***var*** and ***let***?
+//var can be redeclared but cannot redeclare let
+//- What is the difference between **var** and ***const***?
+//const cannot be redeclared or reassigned where var can
+//- What is the difference between **let** and **const**?
+//they both create block scope but let you can reassign const you cannot
+//- What is hoisting?
+//variables are hoisted to the top of the scope in the order they are declred in 
